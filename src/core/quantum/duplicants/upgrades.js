c79ce20b32045868220752ea78715af4cafa20a6
Thien-Hubbing <triplettran56@gmail.com>
2025-10-03 07:57:49 -0400

    misc changes
    Signed-off-by: Thien-Hubbing <triplettran56@gmail.com>

--- src/core/quantum/duplicants/upgrades.js
+++ src/core/quantum/duplicants/upgrades.js
@@ -1,7 +1,8 @@
+// oxlint-disable max-classes-per-file
 import { DC } from "@/core/constants";
 import { RebuyableMechanicState } from "@/core/game-mechanics";
 
-class DuplicantUpgrade extends RebuyableMechanicState {
+class DuplicantUpgradeState extends RebuyableMechanicState {
   constructor() {
     super({
       id: "NormalDuplicantRebuyable",
@@ -57,7 +58,7 @@ class DuplicantUpgrade extends RebuyableMechanicState {
   }
 }
 
-class WorkerLimitRebuyable extends RebuyableMechanicState {
+class WorkerLimitRebuyableState extends RebuyableMechanicState {
   constructor() {
     // How the "workers limit" rebuybale upgrade works is a little strange...
     // If you don't have emperor dimensions, it is limited to 10 workers, otherwise
@@ -66,19 +67,29 @@ class WorkerLimitRebuyable extends RebuyableMechanicState {
     // like a normal upgrade without any special formulas.
     super({
       id: "WorkerLimitRebuyable",
-      cost: () => Decimal.pow(DC.D6, player.quantum.duplicants.workerLimitRebuyable).mul(DC.E49),
+      cost: () => Decimal.pow(DC.D6, scale({
+        baseResource: player.quantum.duplicants.workerLimitRebuyable,
+        scaleMode: SCALING_TYPES.POLYNOMIAL,
+        scaleStart: new Decimal(1000),
+        scalePower: 5,
+      })).mul(DC.E49),
       effect: () => player.quantum.duplicants.workerLimitRebuyable,
       formatEffect: (value) => {
         if (TimeStudy.emperorDimension.isBought) {
           if (value.lt(70)) {
             return `${formatInt(value.mod(10))} ED${formatInt(value.div(10))}`;
           }
+
+          return `${formatInt(value.sub(70))} ED8`;
         }
       },
       description: () => "Reset Replicanti for a Normal Duplicant.",
-      formatCost: value => format(value, 2, 2),
+      formatCost: () => format(WorkerLimitRebuyable.realCost, 2, 2),
     });
   }
+
+  // The currency is the sum of all 3 gluon types, which doesn't work with RebuyableMechanicState
+  // so these are stubbed out appropriately in favour of a custom function
 
   get currency() {
     return Currency.replicanti.value;
@@ -89,15 +100,36 @@ class WorkerLimitRebuyable extends RebuyableMechanicState {
   }
 
   get boughtAmount() {
-    return player.quantum.duplicants.duplicantRebuyable;
+    return player.quantum.duplicants.limitUpgrade;
   }
 
   set boughtAmount(newValue) {
-    player.quantum.duplicants.duplicantRebuyable = newValue;
+    player.quantum.duplicants.limitUpgrade = newValue;
   }
 
   get isAvailableForPurchase() {
     return TimeStudy.emperorDimensions.isBought ? true : this.boughtAmount.lt(10);
+  }
+
+  get canBeBought() {
+    return player.quantum.gluons.rg.gte(this.cost) && player.quantum.gluons.gb.gte(this.cost) && player.quantum.gluons.br.gte(this.cost);
+  }
+
+  costFormula(amount, isInverse = false) {
+    return isInverse
+      ? scale({
+          baseResource: amount.div("1e49").log(6),
+          scaleMode: SCALING_TYPES.POLYNOMIAL,
+          scaleStart: new Decimal(1000),
+          scalePower: 5,
+          isInverted: true,
+        })
+      : Decimal.pow(DC.D6, scale({
+          baseResource: amount,
+          scaleMode: SCALING_TYPES.POLYNOMIAL,
+          scaleStart: new Decimal(1000),
+          scalePower: 5,
+        })).mul(DC.E49);
   }
 
   purchase(amount = 1, auto = false) {
