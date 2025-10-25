55ed22f417af62090fae4ed75acbda08d454229f
Skylafalls <triplettran56@gmail.com>
2025-10-25 18:19:28 -0400

    use esbuild for minification + enable native vite plugins
    Signed-off-by: Skylafalls <triplettran56@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,12 @@ const config = defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  experimental: {
+    enableNativePlugin: true,
+  },
+  build: {
+    minify: "esbuild",
+  },
 });
 
 export default config;
